401791b32023e30e40b7a147ca9f66fed4c5f8f7
hauvm <92607299+hauvm@users.noreply.github.com>
2021-10-30 11:33:08 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -46,8 +46,8 @@ daemon
 maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
+nserver 2606:4700:4700::1111
+nserver 2606:4700:4700::1001
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
