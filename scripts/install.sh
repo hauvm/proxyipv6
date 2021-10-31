d9102440fceae3acc66c6c4839b5e5df6bc90833
hauvm <92607299+hauvm@users.noreply.github.com>
2021-10-31 22:38:07 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,11 +54,11 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth none
+auth strong
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-$(awk -F "/" '{print "auth none\n" \
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
