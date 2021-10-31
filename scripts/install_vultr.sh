f4b98bd18793ff50124a615d5850041a70eeff5d
hauvm <92607299+hauvm@users.noreply.github.com>
2021-11-01 05:38:41 +0700

    Update install_vultr.sh

--- scripts/install_vultr.sh
+++ scripts/install_vultr.sh
@@ -54,10 +54,11 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth iponly strong
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth iponly strong\n" \
-"allow " $1 " 14.250.222.33 * * \n" \
+auth iponly cache
+allow * 14.250.222.33 * *
+
+$(awk -F "/" '{print "auth iponly cache\n" \
+"allow * 14.250.222.33 * * \n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
